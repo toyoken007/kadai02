fd08a2666d74643d2dfc08edfc40c21068006946
toyoken3308 <toyoken007@gmail.com>
2024-02-05 23:35:11 +0900

    スリック読み込み。ファーストビュー途中。

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,28 @@
 //----------------------------------------------------------------------
 //  import
 //----------------------------------------------------------------------
-import { tab } from "./sub.js";
+// import { tab } from "./sub.js";
 
-tab('.panel-a', '.panel-b', '.panel-c');
+// tab('.panel-a', '.panel-b', '.panel-c');
 
 //----------------------------------------------------------------------
 //  jQuery 関数
 //----------------------------------------------------------------------
-$(function(){
-    console.log("aaa");
-});
 
+$(function () {
+    $("#img_slider").slick({
+      arrows: false,/*矢印有無*/
+      dots: false,/*ドット有無*/
+      autoplay: true,
+      autoplayspeed: 3000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      pauseOnDotsHover: false,
+      fade: true,
+      speed: 3000
+  
+    });
+});
 
 //----------------------------------------------------------------------
 //  javascript 関数
